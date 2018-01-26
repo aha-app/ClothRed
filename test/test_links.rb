9b3a9ac215ba9b256e5c8fad1df24b840db52312
Zach Schneider <zach@schneid.io>
2018-01-26 10:47:36 -0500

    Strip out stray a tags from tinymce when converting to textile

--- test/test_links.rb
+++ test/test_links.rb
@@ -7,7 +7,9 @@ class TestClothRedLinks < Test::Unit::TestCase
 
   LINKS_TESTS = [
     ["<a href=\"http://www.rallydev.com\">http://www.rallydev.com</a>", "[http://www.rallydev.com]"],
-    ["<a href=\"http://www.rallydev.com\">Rally Software Development Corp</a>", "[Rally Software Development Corp|http://www.rallydev.com]"]
+    ["<a href=\"http://www.rallydev.com\">Rally Software Development Corp</a>", "[Rally Software Development Corp|http://www.rallydev.com]"],
+    ["<a class=\"mce-item-anchor\" title=\"\" contenteditable=\"false\" name=\"bigheading\"></a>", ''],
+    ["<a class=\"mce-item-anchor\" title=\"\" contenteditable=\"false\" name=\"smallheading\"></a>", '']
   ]
 
   def test_lists
